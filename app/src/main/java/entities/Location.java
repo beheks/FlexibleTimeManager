42e8109512c90d70b03978cb302e868a1aca5d38
Tatiana Schmidt <tatianaschg@gmail.com>
2016-03-28 18:49:24 +0100

    Initial repository interface and entities.

---
+++ app/src/main/java/entities/Location.java
@@ -0,0 +1,13 @@
+package entities;
+
+import java.util.List;
+
+/**
+ * Created by tatianaschmidt on 3/28/16.
+ */
+public class Location {
+    String name;
+
+    List<Rule> ruleList;
+    List<Task> taskList;
+}
