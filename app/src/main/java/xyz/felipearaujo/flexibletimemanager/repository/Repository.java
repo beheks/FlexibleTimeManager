d5c1e96b952161b7ed943c0e6358a37a8031b954
Felipe Araujo <hireme@felipearaujo.xyz>
2016-03-28 19:01:00 +0100

    LocationRepository Interface started

--- app/src/main/java/xyz/felipearaujo/flexibletimemanager/repository/Repository.java
+++
@@ -1,9 +0,0 @@
-package xyz.felipearaujo.flexibletimemanager.repository;
-
-/**
- * Created by tatianaschmidt on 3/28/16.
- */
-public interface Repository {
-    interface Observable buildRepository();
-}
-
