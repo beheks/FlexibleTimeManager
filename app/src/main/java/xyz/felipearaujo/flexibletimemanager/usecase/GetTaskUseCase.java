081db827cbf62b280f8e1bade38e972d534ee81c
Felipe Araujo <me@felipearaujo.xyz>
2016-03-31 19:16:20 +0100

    GetTask started

--- app/src/main/java/xyz/felipearaujo/flexibletimemanager/usecase/GetTaskUseCase.java
+++
@@ -1,9 +0,0 @@
-package xyz.felipearaujo.flexibletimemanager.usecase;
-
-import rx.Observable;
-
-public class GetTaskUseCase extends UseCase {
-    protected Observable buildUseCase() {
-        return null;
-    }
-}
