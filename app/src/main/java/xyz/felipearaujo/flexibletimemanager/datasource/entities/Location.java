95f3019a4ae56a74c501e0daad1438010a4deb7c
Tatiana Schmidt <tatianaschg@gmail.com>
2016-04-24 19:46:28 +0100

    Add settings and main activities.

--- app/src/main/java/xyz/felipearaujo/flexibletimemanager/datasource/entities/Location.java
+++ app/src/main/java/xyz/felipearaujo/flexibletimemanager/datasource/entities/Location.java
@@ -10,9 +10,11 @@ public class Location {
     public static final String TABLE_NAME = "Location";
     public static final String COL_ID = "id";
     public static final String COL_NAME = "name";
+    public static final String COL_RULE_GEOLOCATION = "rule_geolocation";
 
     @Column(COL_ID) long mId;
     @Column(COL_NAME) String mName;
+    @Column(COL_RULE_GEOLOCATION) RuleGeolocation ruleGeolocation;
 
     private List<Rule> mRules;
     private List<Task> mTasks;
