0cf708da1827a51a310fedf9a5d41dcd18565e44
Tatiana Schmidt <tatianaschg@gmail.com>
2016-05-02 21:06:31 +0100

    Add activity to create new location and start creating a list of locations in the main activity.

--- app/src/main/java/xyz/felipearaujo/flexibletimemanager/MainActivity.java
+++ app/src/main/java/xyz/felipearaujo/flexibletimemanager/MainActivity.java
@@ -6,8 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationsAdapter mLocationsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +21,40 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Set up Location view
+        GridView locationsListGrid = (GridView) findViewById(R.id.locations_grid);
+        locationsListGrid.setAdapter(mLocationsAdapter);
+
+        // Action to add a new Location
+        FloatingActionButton addLocationfab = (FloatingActionButton) findViewById(R.id.fab);
+        addLocationfab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Create Location", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
 
+    private class LocationsAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
 }
---
+++ app/src/main/java/xyz/felipearaujo/flexibletimemanager/activity/Create.java
@@ -0,0 +1,15 @@
+package xyz.felipearaujo.flexibletimemanager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xyz.felipearaujo.flexibletimemanager.R;
+
+public class Create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
